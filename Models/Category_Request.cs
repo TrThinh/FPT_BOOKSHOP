259c4a9d1c28ce1ec045130176ceb64098ea966a
TrThinh <TrongThinh2602@gmail.com>
2023-12-17 09:58:15 +0700

    Add controller and view

--- Models/Category_Request.cs
+++ Models/Category_Request.cs
@@ -1,11 +1,6 @@
-using FPT_BOOKSHOP.Models;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FPT_BOOKSHOP.Models
 {
--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
-using System.Text.RegularExpressions; 
+
 
 namespace FPT_BOOKSHOP.Models
 {
